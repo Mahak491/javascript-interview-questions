9c18a8273a875405da4f9597e3da3e602d0c2c8e
Mahak491 <shreemahak01@gmail.com>
2022-08-26 10:15:34 +0530

    1st-interview-ques

---
+++ interview.js
@@ -0,0 +1,19 @@
+console.log("varname",varname);
+var varname;
+varname = "cp";
+
+fn();
+
+function fn(){
+    console.log("i am function");
+}
+
+fn();
+
+fncontainer();
+
+function fncontainer(){
+    console.log("i am function conatiner");
+}
+
+fncontainer();
